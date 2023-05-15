4e4f0eb342908b580eb4c59c0220fa8ad3c9b9da
Hillary <hillaryweaver1@gmail.com>
2023-05-15 14:59:04 -0400

    init

---
+++ script.py
@@ -0,0 +1,12 @@
+# Import libraries
+import codecademylib3
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import RFE
+
+# Load the data
+obesity = pd.read_csv("obesity.csv")
